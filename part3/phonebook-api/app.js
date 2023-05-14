24bb15b722cc528a731cf045661f39dd564c793a
pashapushak1996 <pashapushak1996@gmail.com>
2023-05-14 12:51:23 +0300

    exercises 3.7 - 3.8

--- part3/phonebook-api/app.js
+++ part3/phonebook-api/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const persons = require('./db');
 
 const app = express();
 
 app.use(express.json());
+
+morgan.token('body', function (req, res) {
+  return  JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 
 app.listen(3001, () => {
     console.log('Server is running on 3001 port');
